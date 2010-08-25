5df596b03a30b499333b669e57e110a3e02a28ad
cs <cs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2010-08-25 16:51:56 +0000

    added configuration for maple
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/loop-unrolling@554 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- gui/src/main/java/de/gaalop/gui/ConfigurationPanel.java
+++ gui/src/main/java/de/gaalop/gui/ConfigurationPanel.java
@@ -1,6 +1,7 @@
 package de.gaalop.gui;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -21,6 +22,8 @@ import org.apache.commons.beanutils.BeanUtils;
 
 import de.gaalop.CodeGeneratorPlugin;
 import de.gaalop.ConfigurationProperty;
+import de.gaalop.OptimizationStrategyPlugin;
+import de.gaalop.Plugin;
 import de.gaalop.Plugins;
 
 /**
@@ -46,95 +49,125 @@ public class ConfigurationPanel extends JPanel {
 	private void initialize() {
 		setLayout(new BorderLayout());
 		add(pluginPanes, BorderLayout.CENTER);
+		for (OptimizationStrategyPlugin strategy : Plugins.getOptimizationStrategyPlugins()) {
+			addPluginConfig(strategy);
+		}
 		List<CodeGeneratorPlugin> sortedPlugins = new ArrayList<CodeGeneratorPlugin>(Plugins.getCodeGeneratorPlugins());
 		Collections.sort(sortedPlugins, new PluginSorter());
-		for (final CodeGeneratorPlugin generator : sortedPlugins) {
-			JPanel panel = new JPanel();
-			List<Field> configProperties = new ArrayList<Field>();
-			for (Field field : generator.getClass().getFields()) {
-				if (field.isAnnotationPresent(ConfigurationProperty.class)) {
-					configProperties.add(field);
-				}
-			}
-			for (final Field property : configProperties) {
-				panel.add(new JLabel(property.getName()));
-				try {
-					String value = BeanUtils.getProperty(generator, property.getName());
-					ConfigurationProperty clazz = property.getAnnotation(ConfigurationProperty.class);
-					switch (clazz.type()) {
-					case BOOLEAN:
-						final JCheckBox checkBox = new JCheckBox();
-						checkBox.setSelected(Boolean.parseBoolean(value));
-						checkBox.addActionListener(new ActionListener() {
-							
-							@Override
-							public void actionPerformed(ActionEvent e) {
-								try {
-									BeanUtils.setProperty(generator, property.getName(), checkBox.isSelected());
-								} catch (IllegalAccessException e1) {
-									e1.printStackTrace();
-								} catch (InvocationTargetException e1) {
-									e1.printStackTrace();
-								}
-							}
-							
-						});
-						panel.add(checkBox);
-						break;
-					case NUMBER:
-						final JTextField numberField = new JTextField(value);
-						numberField.addKeyListener(new KeyAdapter() {
-							
-							@Override
-							public void keyReleased(KeyEvent e) {
-								int intVal;
-								if (numberField.getText().length() > 0) {
-									intVal = Integer.parseInt(numberField.getText());
-								} else {
-									intVal = 0;
-								}
-								try {
-									BeanUtils.setProperty(generator, property.getName(), intVal);
-								} catch (IllegalAccessException e1) {
-									e1.printStackTrace();
-								} catch (InvocationTargetException e1) {
-									e1.printStackTrace();
-								}
-							}
-							
-						});
-						break;
-					case TEXT:
-						// fall through to default
-					default:
-						final JTextField textField = new JTextField(value);
-						textField.setColumns(10);
-						textField.addKeyListener(new KeyAdapter() {
-							@Override
-							public void keyReleased(KeyEvent e) {
-								try {
-									BeanUtils.setProperty(generator, property.getName(), textField.getText());
-								} catch (IllegalAccessException e1) {
-									e1.printStackTrace();
-								} catch (InvocationTargetException e1) {
-									e1.printStackTrace();
-								}
-							}
-						});
-						panel.add(textField);
-						break;
-					}
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					e.printStackTrace();
-				} catch (NoSuchMethodException e) {
-					e.printStackTrace();
-				}
-			}
-			pluginPanes.add(generator.getName(), panel);
+		for (CodeGeneratorPlugin generator : sortedPlugins) {
+			addPluginConfig(generator);
 		}
 		pluginPanes.setSelectedIndex(0);
+	}
+
+	private void addOptimizationConfig(OptimizationStrategyPlugin strategy) {
+		JPanel panel = new JPanel();
+		for (final Field property : getConfigurationProperties(strategy)) {
+			
+		}
+	}
+
+	private void addPluginConfig(final Plugin plugin) {
+		JPanel configPanel = new JPanel();
+		configPanel.setLayout(new BorderLayout());
+		List<Field> properties = getConfigurationProperties(plugin);
+		JPanel labels = new JPanel();
+		labels.setLayout(new GridLayout(properties.size(), 1));
+		JPanel fields = new JPanel();
+		fields.setLayout(new GridLayout(properties.size(), 1));
+		configPanel.add(labels, BorderLayout.WEST);
+		configPanel.add(fields, BorderLayout.CENTER);
+		for (final Field property : properties) {
+			labels.add(new JLabel(property.getName()));
+			try {
+				String value = BeanUtils.getProperty(plugin, property.getName());
+				ConfigurationProperty clazz = property.getAnnotation(ConfigurationProperty.class);
+				switch (clazz.type()) {
+				case BOOLEAN:
+					final JCheckBox checkBox = new JCheckBox();
+					checkBox.setSelected(Boolean.parseBoolean(value));
+					checkBox.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							try {
+								BeanUtils.setProperty(plugin, property.getName(), checkBox.isSelected());
+							} catch (IllegalAccessException e1) {
+								e1.printStackTrace();
+							} catch (InvocationTargetException e1) {
+								e1.printStackTrace();
+							}
+						}
+						
+					});
+					fields.add(checkBox);
+					break;
+				case NUMBER:
+					final JTextField numberField = new JTextField(value);
+					numberField.addKeyListener(new KeyAdapter() {
+						
+						@Override
+						public void keyReleased(KeyEvent e) {
+							int intVal;
+							if (numberField.getText().length() > 0) {
+								intVal = Integer.parseInt(numberField.getText());
+							} else {
+								intVal = 0;
+							}
+							try {
+								BeanUtils.setProperty(plugin, property.getName(), intVal);
+							} catch (IllegalAccessException e1) {
+								e1.printStackTrace();
+							} catch (InvocationTargetException e1) {
+								e1.printStackTrace();
+							}
+						}
+						
+					});
+					fields.add(numberField);
+					break;
+				case TEXT:
+					// fall through to default
+				default:
+					final JTextField textField = new JTextField(value);
+					textField.addKeyListener(new KeyAdapter() {
+						@Override
+						public void keyReleased(KeyEvent e) {
+							try {
+								BeanUtils.setProperty(plugin, property.getName(), textField.getText());
+							} catch (IllegalAccessException e1) {
+								e1.printStackTrace();
+							} catch (InvocationTargetException e1) {
+								e1.printStackTrace();
+							}
+						}
+					});
+					fields.add(textField);
+					break;
+				}
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			}
+		}
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+		panel.add(configPanel, BorderLayout.NORTH);
+		panel.add(new JPanel(), BorderLayout.CENTER);
+		pluginPanes.add(plugin.getName(), panel);
+	}
+
+	private List<Field> getConfigurationProperties(Plugin plugin) {
+		List<Field> configProperties = new ArrayList<Field>();
+		for (Field field : plugin.getClass().getFields()) {
+			if (field.isAnnotationPresent(ConfigurationProperty.class)) {
+				configProperties.add(field);
+			}
+		}
+		return configProperties;
 	}
 
 }
