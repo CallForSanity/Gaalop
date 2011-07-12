69b86e2d30b4e7407f907c77a3e614fe101467a0
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2011-07-12 22:14:37 +0000

    TBA: Maxima Plugin. Commit new tests
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@774 4235ee25-d3ef-403b-b6ed-692c26853d4b

---
+++ tba/src/test/java/de/gaalop/tba/generatedTests/CircleNoVars.java
@@ -0,0 +1,40 @@
+package de.gaalop.tba.generatedTests;
+
+import java.util.HashMap;
+
+public class CircleNoVars implements GAProgram {
+	// input variables
+
+	// output variables
+	private float m_1;
+	private float r_0;
+	private float m_2;
+
+	@Override
+	public float getValue(String varName) {
+		if (varName.equals("m_1")) return m_1;
+		if (varName.equals("r_0")) return r_0;
+		if (varName.equals("m_2")) return m_2;
+		return 0.0f;
+	}
+
+	@Override
+	public HashMap<String,Float> getValues() {
+		HashMap<String,Float> result = new HashMap<String,Float>();
+		result.put("m_1",m_1);
+		result.put("r_0",r_0);
+		result.put("m_2",m_2);
+		return result;
+	}
+	@Override
+	public boolean setValue(String varName, float value) {
+		return false;
+	}
+	
+	@Override
+	public void calculate() {
+		m_1 = 1.7727273f;
+		m_2 = 4.863636f;
+		r_0 = 4.314592f;
+	}
+}
