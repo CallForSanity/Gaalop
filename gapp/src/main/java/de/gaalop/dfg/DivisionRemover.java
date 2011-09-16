66f5da43a25612b355b2922747213b363ae6ecff
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2011-09-16 16:12:59 +0000

    Adding hints on errors
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@864 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- gapp/src/main/java/de/gaalop/dfg/DivisionRemover.java
+++ gapp/src/main/java/de/gaalop/dfg/DivisionRemover.java
@@ -8,7 +8,7 @@ import de.gaalop.api.dfg.DFGNodeTypeGetter;
  * with the GA pendant.
  *
  * If a and b are i.e. multivectors and a/b exists in the DFG,
- * then it will be replaced with a*(b/(a.(a~))), where ~ is the reverse.
+ * then it will be replaced with a*(b/(b.(b~))), where ~ is the reverse.
  *
  * @author Christian Steinmetz
  */
