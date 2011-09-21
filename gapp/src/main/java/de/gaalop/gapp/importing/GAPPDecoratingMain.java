ac3d46bf032f203a7152ab91a3c402c1d9b0b3f7
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2011-09-21 21:26:02 +0000

    GAPP: Inputvalues assignments are now at the beginn of the GAPP code
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@883 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- gapp/src/main/java/de/gaalop/gapp/importing/GAPPDecoratingMain.java
+++ gapp/src/main/java/de/gaalop/gapp/importing/GAPPDecoratingMain.java
@@ -2,10 +2,22 @@ package de.gaalop.gapp.importing;
 
 import de.gaalop.OptimizationException;
 import de.gaalop.cfg.ControlFlowGraph;
+import de.gaalop.dfg.MultivectorComponent;
+import de.gaalop.dfg.Variable;
+import de.gaalop.gapp.GAPP;
+import de.gaalop.gapp.Selector;
+import de.gaalop.gapp.Selectorset;
+import de.gaalop.gapp.Variableset;
+import de.gaalop.gapp.instructionSet.GAPPAssignMv;
+import de.gaalop.gapp.instructionSet.GAPPResetMv;
+import de.gaalop.gapp.variables.GAPPMultivector;
+import de.gaalop.gapp.variables.GAPPScalarVariable;
 import de.gaalop.tba.Plugin;
 import de.gaalop.tba.UseAlgebra;
 import de.gaalop.tba.cfgImport.BaseVectorChecker;
 import de.gaalop.tba.cfgImport.CFGImporterFacade;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Facade class to decorate the ControlFlowGraph in a GAPP ControlFlowGraph
@@ -31,11 +43,46 @@ public class GAPPDecoratingMain {
         CFGImporterFacade facade = new CFGImporterFacade(plugin);
         facade.importGraph(graph);
 
+        GAPP gappStart = new GAPP();
+        assignInputVariables(graph, gappStart);
+
         // import now the graph in GAPP
-        GAPPDecorator vCFG = new GAPPDecorator();
+        GAPPDecorator vCFG = new GAPPDecorator(gappStart);
         graph.accept(vCFG);
 
         return graph;
     }
+
+    private void assignInputVariables(ControlFlowGraph graph, GAPP gappStart) {
+        LinkedList<Variable> toDo = new LinkedList<Variable>(graph.getInputVariables());
+
+        GAPPMultivector inputsMv = new GAPPMultivector("inputsMv");
+        gappStart.addInstruction(new GAPPResetMv(inputsMv));
+
+        HashMap<Variable,MultivectorComponent> map = new HashMap<Variable, MultivectorComponent>();
+
+        Selectorset selSet = new Selectorset();
+        Variableset varSet = new Variableset();
+        int slotNo = 0;
+        for (Variable var: toDo) {
+            selSet.add(new Selector(slotNo, (byte) 1));
+            varSet.add(new GAPPScalarVariable(var.getName()));
+            map.put(var, new MultivectorComponent("inputsMv", slotNo));
+            slotNo++;
+        }
+   
+        gappStart.addInstruction(new GAPPAssignMv(inputsMv, selSet, varSet));
+
+        
+        while (!toDo.isEmpty()) {
+            Variable curVar = toDo.removeFirst();
+            ReplaceVisitor replaceVisitor = new ReplaceVisitor(curVar, map.get(curVar));
+            graph.accept(replaceVisitor);
+        }
+    }
+
+
+
+
 
 }
--- gapp/src/main/java/de/gaalop/gapp/importing/GAPPDecorator.java
+++ gapp/src/main/java/de/gaalop/gapp/importing/GAPPDecorator.java
@@ -2,6 +2,7 @@ package de.gaalop.gapp.importing;
 
 import de.gaalop.cfg.AssignmentNode;
 import de.gaalop.cfg.EmptyControlFlowVisitor;
+import de.gaalop.cfg.StartNode;
 import de.gaalop.dfg.MultivectorComponent;
 import de.gaalop.gapp.GAPP;
 import de.gaalop.gapp.importing.parallelObjects.DotProduct;
@@ -20,6 +21,11 @@ public class GAPPDecorator extends EmptyControlFlowVisitor {
     private HashSet<String> createdGAPPVariables = new HashSet<String>();
 
     private GAPPCreator gappCreator = new GAPPCreator(null);
+    private GAPP gappStart;
+
+    public GAPPDecorator(GAPP gappStart) {
+        this.gappStart = gappStart;
+    }
 
     private GAPPMultivector createNewMultivector(String name) {
         return new GAPPMultivector(name);
@@ -31,6 +37,10 @@ public class GAPPDecorator extends EmptyControlFlowVisitor {
         GAPP gapp = new GAPP();
         node.setGAPP(gapp);
         gappCreator.setGapp(gapp);
+        if (gappStart != null) {
+            gapp.addGAPP(gappStart);
+            gappStart = null;
+        }
 
         // remember the name of the current destination multivector component
         MultivectorComponent mvC = (MultivectorComponent) node.getVariable();
@@ -43,23 +53,16 @@ public class GAPPDecorator extends EmptyControlFlowVisitor {
             createdGAPPVariables.add(name);
         }
 
-        System.out.println();
-        System.out.println(node.getVariable().toString());
-        System.out.println(node.getValue().toString());
-
-
         // process expression
         ExpressionCollector collector = new ExpressionCollector();
         node.getValue().accept(collector);
         ParallelObject parallelObject = collector.getResultValue();
 
-        System.out.println(parallelObject.toString());
 
         DotProductsFinder finder = new DotProductsFinder();
         DotProduct newDot = (DotProduct) parallelObject.accept(finder, null);
         if (newDot != null)
             parallelObject = newDot;
-        System.out.println(parallelObject.toString());
 
         //process further: create GAPP from the ParallelObject data structure
         GAPPMultivectorComponent gMvC = new GAPPMultivectorComponent(mvC.getName(), mvC.getBladeIndex());
