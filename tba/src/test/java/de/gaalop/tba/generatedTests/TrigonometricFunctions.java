4c35c0ceb60870024fdde6c9566695360b0225b3
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2011-10-14 13:31:37 +0000

    TBA: fix bug in creating inverse, part 2
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@924 4235ee25-d3ef-403b-b6ed-692c26853d4b

---
+++ tba/src/test/java/de/gaalop/tba/generatedTests/TrigonometricFunctions.java
@@ -0,0 +1,127 @@
+package de.gaalop.tba.generatedTests;
+
+import java.util.HashMap;
+
+public class TrigonometricFunctions implements GAProgram {
+	// input variables
+
+	// output variables
+	private float r$0;
+	private float r$1;
+	private float r$2;
+	private float r$3;
+	private float r$4;
+	private float r$5;
+	private float r$6;
+	private float r$7;
+	private float r$8;
+	private float r$9;
+	private float r$10;
+	private float r$11;
+	private float r$12;
+	private float r$13;
+	private float r$14;
+	private float r$15;
+	private float r$16;
+	private float r$17;
+	private float r$18;
+	private float r$19;
+	private float r$20;
+	private float r$21;
+	private float r$22;
+	private float r$23;
+	private float r$24;
+	private float r$25;
+	private float r$26;
+	private float r$27;
+	private float r$28;
+	private float r$29;
+	private float r$30;
+	private float r$31;
+
+	@Override
+	public float getValue(String varName) {
+		if (varName.equals("r$0")) return r$0;
+		if (varName.equals("r$1")) return r$1;
+		if (varName.equals("r$2")) return r$2;
+		if (varName.equals("r$3")) return r$3;
+		if (varName.equals("r$4")) return r$4;
+		if (varName.equals("r$5")) return r$5;
+		if (varName.equals("r$6")) return r$6;
+		if (varName.equals("r$7")) return r$7;
+		if (varName.equals("r$8")) return r$8;
+		if (varName.equals("r$9")) return r$9;
+		if (varName.equals("r$10")) return r$10;
+		if (varName.equals("r$11")) return r$11;
+		if (varName.equals("r$12")) return r$12;
+		if (varName.equals("r$13")) return r$13;
+		if (varName.equals("r$14")) return r$14;
+		if (varName.equals("r$15")) return r$15;
+		if (varName.equals("r$16")) return r$16;
+		if (varName.equals("r$17")) return r$17;
+		if (varName.equals("r$18")) return r$18;
+		if (varName.equals("r$19")) return r$19;
+		if (varName.equals("r$20")) return r$20;
+		if (varName.equals("r$21")) return r$21;
+		if (varName.equals("r$22")) return r$22;
+		if (varName.equals("r$23")) return r$23;
+		if (varName.equals("r$24")) return r$24;
+		if (varName.equals("r$25")) return r$25;
+		if (varName.equals("r$26")) return r$26;
+		if (varName.equals("r$27")) return r$27;
+		if (varName.equals("r$28")) return r$28;
+		if (varName.equals("r$29")) return r$29;
+		if (varName.equals("r$30")) return r$30;
+		if (varName.equals("r$31")) return r$31;
+		return 0.0f;
+	}
+
+	@Override
+	public HashMap<String,Float> getValues() {
+		HashMap<String,Float> result = new HashMap<String,Float>();
+		result.put("r$0",r$0);
+		result.put("r$1",r$1);
+		result.put("r$2",r$2);
+		result.put("r$3",r$3);
+		result.put("r$4",r$4);
+		result.put("r$5",r$5);
+		result.put("r$6",r$6);
+		result.put("r$7",r$7);
+		result.put("r$8",r$8);
+		result.put("r$9",r$9);
+		result.put("r$10",r$10);
+		result.put("r$11",r$11);
+		result.put("r$12",r$12);
+		result.put("r$13",r$13);
+		result.put("r$14",r$14);
+		result.put("r$15",r$15);
+		result.put("r$16",r$16);
+		result.put("r$17",r$17);
+		result.put("r$18",r$18);
+		result.put("r$19",r$19);
+		result.put("r$20",r$20);
+		result.put("r$21",r$21);
+		result.put("r$22",r$22);
+		result.put("r$23",r$23);
+		result.put("r$24",r$24);
+		result.put("r$25",r$25);
+		result.put("r$26",r$26);
+		result.put("r$27",r$27);
+		result.put("r$28",r$28);
+		result.put("r$29",r$29);
+		result.put("r$30",r$30);
+		result.put("r$31",r$31);
+		return result;
+	}
+	@Override
+	public boolean setValue(String varName, float value) {
+		return false;
+	}
+	
+	@Override
+	public void calculate() {
+		r$0 = 1.0f; // 1.0;
+	}
+
+
+}
