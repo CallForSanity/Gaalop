c5da5fed777947bbafffa27514d31acd6567c1c8
pc <pc@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2011-10-25 12:00:10 +0000

    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@942 4235ee25-d3ef-403b-b6ed-692c26853d4b

---
+++ codegen-gappopencl/src/main/java/de/gaalop/gappopencl/GAPPOpenCLVisitor.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.gaalop.gappopencl;
+
+import de.gaalop.gapp.instructionSet.GAPPAssignMv;
+import de.gaalop.gapp.instructionSet.GAPPAssignVector;
+import de.gaalop.gapp.instructionSet.GAPPCalculateMv;
+import de.gaalop.gapp.instructionSet.GAPPCalculateMvCoeff;
+import de.gaalop.gapp.instructionSet.GAPPDotVectors;
+import de.gaalop.gapp.instructionSet.GAPPResetMv;
+import de.gaalop.gapp.instructionSet.GAPPSetMv;
+import de.gaalop.gapp.instructionSet.GAPPSetVector;
+
+/**
+ *
+ * @author patrick
+ */
+public class GAPPOpenCLVisitor extends de.gaalop.gapp.visitor.CFGGAPPVisitor {
+    
+    private StringBuilder result = new StringBuilder();
+    
+    @Override
+    public Object visitAssignMv(GAPPAssignMv gappAssignMv, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitDotVectors(GAPPDotVectors gappDotVectors, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitResetMv(GAPPResetMv gappResetMv, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitSetMv(GAPPSetMv gappSetMv, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitSetVector(GAPPSetVector gappSetVector, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitCalculateMv(GAPPCalculateMv gappCalculateMv, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitAssignVector(GAPPAssignVector gappAssignVector, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object visitCalculateMvCoeff(GAPPCalculateMvCoeff gappCalculateMvCoeff, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    String getCode() {
+        return "";
+    }
+}
---
+++ codegen-gappopencl/src/main/java/de/gaalop/gappopencl/Plugin.java
@@ -0,0 +1,75 @@
+package de.gaalop.gappopencl;
+
+import de.gaalop.CodeGenerator;
+import de.gaalop.CodeGeneratorPlugin;
+import de.gaalop.ConfigurationProperty;
+import de.gaalop.ConfigurationProperty.Type;
+import de.gaalop.Notifications;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Observable;
+
+/**
+ * This class implements the Plugin interface for Gaalop.
+ */
+public class Plugin extends Observable implements CodeGeneratorPlugin {
+
+    private Log log = LogFactory.getLog(Plugin.class);
+
+    private Image icon;
+
+    @ConfigurationProperty(type = Type.BOOLEAN)
+    public boolean standalone = true;
+
+    public Plugin() {
+        URL url = getClass().getResource("icon.png");
+        if (url != null) {
+            try {
+                icon = ImageIO.read(url);
+            } catch (IOException e) {
+                log.error("Unable to read plugin icon " + url);
+            }
+        } else {
+            log.warn("Unable to find plugin icon!");
+        }
+    }
+
+    public void setStandalone(boolean standalone) {
+		this.standalone = standalone;
+	}
+
+    public boolean getStandalone() {
+		return standalone;
+	}
+
+    @Override
+    public CodeGenerator createCodeGenerator() {
+        return new GAPPOpenCLCodeGenerator(this);
+    }
+
+    @Override
+    public String getName() {
+        return "GAPP OpenCL";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This plugin generates C/C++ code.";
+    }
+
+    @Override
+    public Image getIcon() {
+        return icon;
+    }
+
+    void notifyError(Throwable error) {
+    	setChanged();
+    	notifyObservers(new Notifications.Error(error));
+    }
+}
