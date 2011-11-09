dbd09af2cfe58a2a9ac70c130be406bd01d1a494
pc <pc@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2011-11-09 09:55:29 +0000

    compressed fixed
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@991 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- codegen-compressed/src/main/java/de/gaalop/compressed/CompressedVisitor.java
+++ codegen-compressed/src/main/java/de/gaalop/compressed/CompressedVisitor.java
@@ -16,137 +16,244 @@ import org.apache.commons.logging.LogFactory;
  */
 public class CompressedVisitor extends de.gaalop.gaalet.output.CppVisitor {
 
-        protected boolean gcdMetaInfo = true;
+    protected boolean gcdMetaInfo = true;
 
-	public CompressedVisitor(boolean standalone) {
-		super(standalone);
-	}
+    public CompressedVisitor(boolean standalone) {
+        super(standalone);
+    }
 
-//	@Override
-//	public void visit(StartNode node) {
-//		graph = node.getGraph();
-//
-//		FindStoreOutputNodes findOutput = new FindStoreOutputNodes();
-//		graph.accept(findOutput);
-//		for (StoreResultNode var : findOutput.getNodes()) {
-//			String outputName = var.getValue().getName() + "_out";
-//
-//			outputNamesMap.put(var, outputName);
-//		}
-//		if (standalone) {
-//			code.append("void calculate(");
-//
-//			// Input Parameters
-//			List<Variable> inputParameters = sortVariables(graph.getInputVariables());
-//			for (Variable var : inputParameters) {
-//				code.append("float "); // The assumption here is that they all are normal scalars
-//				printVarName(var.getName());
-//				code.append(", ");
-//			}
-//
-//			for (StoreResultNode var : findOutput.getNodes()) {
-//				code.append("float **");
-//				printVarName(outputNamesMap.get(var));
-//				code.append(", ");
-//			}
-//
-//			if (!graph.getInputVariables().isEmpty() || !findOutput.getNodes().isEmpty()) {
-//				code.setLength(code.length() - 2);
-//			}
-//
-//			code.append(") {\n");
-//			indentation++;
-//		}
-//
-//		handleLocalVariables();
-//		createVectorSet(findOutput);
-//		node.getSuccessor().accept(this);
-//	}
-	
-	/**	
-	* Declare local variables
-	*	 but first local variables in a set, so we reduce redundancy
-	*/
-	@Override
-	protected void handleLocalVariables() {
-                 System.out.println("blub");
-		for (Variable var : graph.getLocalVariables()) {
-                        // count number of components
-			FieldsUsedVisitor fieldVisitor = new FieldsUsedVisitor(var.getName());
-			graph.accept(fieldVisitor);
-			final int size = fieldVisitor.getMultiVector().getGaalopBlades().size();
-                        if(size <= 0)
-                            continue;
-                        System.out.println(size);
+    /*
+     * disable gaalet comments
+     */
+    @Override
+    public void visit(StartNode node) {
+        graph = node.getGraph();
 
-			// GCD definition
-			if(gcdMetaInfo) {
-           			appendIndentation();
-				code.append("//#pragma gcd multivector ");
-				code.append(var.getName());
-				code.append('\n');
-			}
+        FindStoreOutputNodes findOutput = new FindStoreOutputNodes();
+        graph.accept(findOutput);
+        for (StoreResultNode var : findOutput.getNodes()) {
+            String outputName = var.getValue().getName() + "_out";
 
-			// standard definition
-           		appendIndentation();
-			code.append(variableType + ' ' + var.getName());
-			code.append("[" + size + "]");
-			code.append(";\n");
-		}
+            outputNamesMap.put(var, outputName);
+        }
+        if (standalone) {
+            code.append("void calculate(");
 
-		if (!graph.getLocalVariables().isEmpty()) {
-			code.append("\n");
-		}				
-	}
-        
-//	@Override
-//	public void visit(AssignmentNode node) {
-//		if (assigned.contains(node.getVariable().getName())) {
-//			log.warn("Reuse of variable " + node.getVariable().getName()
-//				+ ". Make sure to reset this variable or use another name.");
-//			code.append("\n");
-//			appendIndentation();
-//			code.append("// Warning: reuse of variable ");
-//			code.append(node.getVariable().getName());
-//			code.append(".\n");
-//			appendIndentation();
-//			code.append("// Make sure to reset this variable or use another name.\n");
-//			assigned.remove(node.getVariable().getName());
-//		}
-//
-//		appendIndentation();
-//                 System.out.println("blob");
-//		node.getVariable().accept(this);
-//		code.append(" = ");
-//		node.getValue().accept(this);
-//		code.append(";\n");
-//
-//		node.getSuccessor().accept(this);
-//	}
+            // Input Parameters
+            List<Variable> inputParameters = sortVariables(graph.getInputVariables());
+            for (Variable var : inputParameters) {
+                code.append("float "); // The assumption here is that they all are normal scalars
+                printVarName(var.getName());
+                code.append(", ");
+            }
+
+            for (StoreResultNode var : findOutput.getNodes()) {
+                code.append("float **");
+                printVarName(outputNamesMap.get(var));
+                code.append(", ");
+            }
+
+            if (!graph.getInputVariables().isEmpty() || !findOutput.getNodes().isEmpty()) {
+                code.setLength(code.length() - 2);
+            }
+
+            code.append(") {\n");
+            indentation++;
+        }
+
+        handleLocalVariables();
+        createVectorSet(findOutput);
+        node.getSuccessor().accept(this);
+    }
+
+    /**	
+     * Declare local variables
+     *	 but first local variables in a set, so we reduce redundancy
+     */
+    @Override
+    protected void handleLocalVariables() {
+        for (Variable var : graph.getLocalVariables()) {
+            // count number of components
+            FieldsUsedVisitor fieldVisitor = new FieldsUsedVisitor(var.getName());
+            graph.accept(fieldVisitor);
+            final int size = fieldVisitor.getMultiVector().getGaalopBlades().size();
+            if (size <= 0)
+                continue;
+
+            // GCD definition
+            if (gcdMetaInfo) {
+                appendIndentation();
+                code.append("//#pragma gcd multivector ");
+                code.append(var.getName());
+                code.append('\n');
+            }
+
+            // standard definition
+            appendIndentation();
+            code.append(variableType + ' ' + var.getName());
+            code.append("[" + size + "]");
+            code.append(";\n");
+        }
+
+        if (!graph.getLocalVariables().isEmpty()) {
+            code.append("\n");
+        }
+    }
+
+    @Override
+    public void visit(AssignmentNode node) {
+        if (assigned.contains(node.getVariable().getName())) {
+            log.warn("Reuse of variable " + node.getVariable().getName()
+                    + ". Make sure to reset this variable or use another name.");
+            code.append("\n");
+            appendIndentation();
+            code.append("// Warning: reuse of variable ");
+            code.append(node.getVariable().getName());
+            code.append(".\n");
+            appendIndentation();
+            code.append("// Make sure to reset this variable or use another name.\n");
+            assigned.remove(node.getVariable().getName());
+        }
+
+        appendIndentation();
+        node.getVariable().accept(new DetourVisitor());
+        code.append(" = ");
+        node.getValue().accept(this);
+        code.append(";\n");
+
+        node.getSuccessor().accept(this);
+    }
+
+    protected class DetourVisitor implements de.gaalop.dfg.ExpressionVisitor {
 
         @Override
         public void visit(MultivectorComponent component) {
-                 System.out.println("blab");
-                // get blade pos in array
-                final String name = component.getName().replace(suffix, "");
-                int pos = -1;
-                for (GaaletMultiVector vec : vectorSet) {
-                        if(name.equals(vec.getName()))
-                                pos = vec.getBladePosInArray(component.getBladeIndex());
+            // get blade pos in array
+            final String name = component.getName().replace(suffix, "");
+            int pos = -1;
+            for (GaaletMultiVector vec : vectorSet) {
+                if (name.equals(vec.getName())) {
+                    pos = vec.getBladePosInArray(component.getBladeIndex());
                 }
+            }
 
-                // GCD definition
-                final String componentName = name + '[' + pos + ']';
-                if(gcdMetaInfo)
-                {
-                        code.append("//#pragma gcd multivector_component ");
-                        code.append(componentName);
-                        code.append(' ');
-                        code.append(component.getBladeName());
-                        code.append('\n');
-                }
+            // GCD definition
+            final String componentName = name + '[' + pos + ']';
+            if (gcdMetaInfo) {
+                code.append("//#pragma gcd multivector_component ");
+                code.append(componentName);
+                code.append(' ');
+                code.append(component.getBladeName());
+                code.append('\n');
+            }
 
-                // standard definition
-                printVarName(componentName);
+            // standard definition
+            code.append(componentName);
         }
+
+        @Override
+        public void visit(Subtraction node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Addition node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Division node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(InnerProduct node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Multiplication node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(MathFunctionCall node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Variable node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Exponentiation node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(FloatConstant node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(OuterProduct node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(BaseVector node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Negation node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Reverse node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(LogicalOr node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(LogicalAnd node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(LogicalNegation node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Equality node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Inequality node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(Relation relation) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(FunctionArgument node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void visit(MacroCall node) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
 }
