e8881108789bd9383e3d71d3653f96850db0a383
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2012-01-26 20:19:33 +0000

    optimized java productcomputer
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@1115 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- tba/src/main/java/de/gaalop/productComputer/Main.java
+++ tba/src/main/java/de/gaalop/productComputer/Main.java
@@ -2,12 +2,12 @@ package de.gaalop.productComputer;
 
 import de.gaalop.algebra.AlStrategy;
 import de.gaalop.cfg.AlgebraDefinitionFile;
-import de.gaalop.productComputer.AlgebraPC;
+import de.gaalop.tba.IMultTable;
 import de.gaalop.tba.MultTableAbsDirectComputer;
+import de.gaalop.tba.MultTableImpl;
 import de.gaalop.tba.table.BitIO.BitReader;
 import de.gaalop.tba.table.BitIO.BitWriter;
 import de.gaalop.tba.table.TableCompressed;
-import de.gaalop.tba.table.TableHumanReadable;
 import de.gaalop.tba.table.TableReaderIO;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -21,21 +21,42 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
             ProductComputer productComputer = new ProductComputer();
-            InputStream inputStream = AlStrategy.class.getResourceAsStream("algebra/5d/definition.csv");
+            InputStream inputStream = AlStrategy.class.getResourceAsStream("algebra/9d/definition.csv");
             AlgebraDefinitionFile alFile = new AlgebraDefinitionFile();
             alFile.loadFromFile(inputStream);
             AlgebraPC algebraPC = new AlgebraPC(alFile);
             productComputer.initialize(algebraPC);
 
+
+            //precalculate tables because of double use in TableCompressed
             int bladeCount = (int) Math.pow(2, algebraPC.base.length);
+
+            MultTableImpl inner = new MultTableImpl();
+            inner.createTable(bladeCount);
+            MultTableImpl outer = new MultTableImpl();
+            outer.createTable(bladeCount);
+            MultTableImpl geo = new MultTableImpl();
+            geo.createTable(bladeCount);
+
+            IMultTable innerLive = new MultTableAbsDirectComputer(alFile, new InnerProductCalculator());
+            IMultTable outerLive = new MultTableAbsDirectComputer(alFile, new OuterProductCalculator());
+            IMultTable geoLive = new MultTableAbsDirectComputer(alFile, new GeoProductCalculator());
+            
+            for (int i=0;i<bladeCount;i++)
+                for (int j=0;j<bladeCount;j++) {
+                    inner.setProduct(i, j, innerLive.getProduct(i, j));
+                    outer.setProduct(i, j, outerLive.getProduct(i, j));
+                    geo.setProduct(i, j, geoLive.getProduct(i, j));
+                }
+            
             
             TableReaderIO io = new TableCompressed(new BitReader(), new BitWriter());
             io.writeToFile(
                     bladeCount,
                     algebraPC.base.length,
-                    new MultTableAbsDirectComputer(alFile, new InnerProductCalculator()),
-                    new MultTableAbsDirectComputer(alFile, new OuterProductCalculator()),
-                    new MultTableAbsDirectComputer(alFile, new GeoProductCalculator()),
+                    inner,
+                    outer,
+                    geo,
                     new FileOutputStream("products.csv")
                     );
 
