300e8aa839eaa6dd5f9d8519df84cd57f2b8d1ee
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2012-01-28 15:46:51 +0000

    compressed product table: used bytes for storage, also in header
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@1120 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- tba/src/main/java/de/gaalop/tba/table/TableCompressed.java
+++ tba/src/main/java/de/gaalop/tba/table/TableCompressed.java
@@ -5,8 +5,13 @@ import de.gaalop.tba.table.BitIO.AbsBitReader;
 import de.gaalop.tba.BladeRef;
 import de.gaalop.tba.IMultTable;
 import de.gaalop.tba.Multivector;
+import de.gaalop.tba.table.BitIO.BitReader;
+import de.gaalop.tba.table.BitIO.BitWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -38,7 +43,7 @@ public class TableCompressed implements TableReaderIO {
             }
             int dimension = in.readByte();
             int bladeCount = (int) Math.pow(2,dimension);
-            int bitCount = in.readInt();
+            int bitCount = in.readByte();
 
             reader.setDataInputStream(in);
 
@@ -91,54 +96,72 @@ public class TableCompressed implements TableReaderIO {
         }
     }
 
-    /**
-     * Determines the maximum number of summands
-     * @param bladeCount The number of blades
-     * @param innerTable The inner product table
-     * @param outerTable The outer product table
-     * @param geoTable The geometric product table
-     * @return The maximum number of summands
-     */
-    private int getMaximalNumberOfSummands(int bladeCount, IMultTable innerTable, IMultTable outerTable, IMultTable geoTable) {
-        int maxNumber = 0;
-        for (int i=0;i<bladeCount;i++)
-            for (int j=0;j<bladeCount;j++) {
-                maxNumber = Math.max(maxNumber,innerTable.getProduct(i, j).getBlades().size());
-                maxNumber = Math.max(maxNumber,outerTable.getProduct(i, j).getBlades().size());
-                maxNumber = Math.max(maxNumber,geoTable.getProduct(i, j).getBlades().size());
-            }
-        return maxNumber;
-    }
-
     @Override
     public void writeToFile(int bladeCount, int dimension, IMultTable innerTable, IMultTable outerTable, IMultTable geoTable, OutputStream outputStream) {
         try {
-            int maxNumber = getMaximalNumberOfSummands(bladeCount, innerTable, outerTable, geoTable);
+            BitWriter w = new BitWriter();
 
-            int number = 2;
-            int bitCount = 1;
-            while (number < maxNumber+1) {
-                bitCount++;
-                number *= 2;
-            }
-            
-            DataOutputStream out = new DataOutputStream(outputStream);
-            out.writeByte(1); //format
-            out.writeByte(dimension); //dimension
-            out.writeInt(bitCount);
+            int bitCount = 4;
+            File tempFile = File.createTempFile("TableCreator", "txt");
+            DataOutputStream out = new DataOutputStream(new FileOutputStream(tempFile));
 
-            writer.setDataOutputStream(out);
+            w.setDataOutputStream(out);
+
+            int maxNumber = 0;
 
             for (int i=0;i<bladeCount;i++)
                 for (int j=0;j<bladeCount;j++) {
-                    writeMultivector(innerTable.getProduct(i, j), dimension, writer, bitCount);
-                    writeMultivector(outerTable.getProduct(i, j), dimension, writer, bitCount);
-                    writeMultivector(geoTable.getProduct(i, j), dimension, writer, bitCount);
+                    Multivector innerM = innerTable.getProduct(i, j);
+                    Multivector outerM = outerTable.getProduct(i, j);
+                    Multivector geoM = geoTable.getProduct(i, j);
+
+                    maxNumber = Math.max(maxNumber, innerM.getBlades().size());
+                    maxNumber = Math.max(maxNumber, outerM.getBlades().size());
+                    maxNumber = Math.max(maxNumber, geoM.getBlades().size());
+
+                    writeMultivector(innerM, dimension, w, bitCount);
+                    writeMultivector(outerM, dimension, w, bitCount);
+                    writeMultivector(geoM, dimension, w, bitCount);
+                }
+
+            w.finish();
+            out.close();
+
+            // ===== 
+
+            out = new DataOutputStream(outputStream);
+
+            DataInputStream in = new DataInputStream(new FileInputStream(tempFile));
+
+            BitReader r = new BitReader();
+            r.setDataInputStream(in);
+
+
+            int number = 2;
+            int bitCount2 = 1;
+            while (number < maxNumber+1) {
+                bitCount2++;
+                number *= 2;
+            }
+
+
+            out.writeByte(1); //format
+            out.writeByte(dimension); //dimension
+            out.writeByte(bitCount2);
+
+            writer.setDataOutputStream(out);
+
+           for (int i=0;i<bladeCount;i++)
+                for (int j=0;j<bladeCount;j++) {
+                    writeMultivector(readMultivector(r, dimension, bitCount), dimension, writer, bitCount2);
+                    writeMultivector(readMultivector(r, dimension, bitCount), dimension, writer, bitCount2);
+                    writeMultivector(readMultivector(r, dimension, bitCount), dimension, writer, bitCount2);
                 }
 
             writer.finish();
+            in.close();
+            tempFile.delete();
 
-            out.close();
         } catch (IOException ex) {
             Logger.getLogger(TableCompressed.class.getName()).log(Level.SEVERE, null, ex);
         }
