f6098c4855fa2ef76c3356d0d06652aaeddce556
chs <chs@4235ee25-d3ef-403b-b6ed-692c26853d4b>
2012-02-25 20:15:42 +0000

    CVisualizer test sucessful
    git-svn-id: svn+ssh://erebor.esa.informatik.tu-darmstadt.de/home/wimi/svn/GaalopCompiler/gaalop-2.0/branches/tbaAndGapp@1172 4235ee25-d3ef-403b-b6ed-692c26853d4b

--- CVisualizer/TestGlut/FromGaalop.h
+++ CVisualizer/TestGlut/FromGaalop.h
@@ -26,8 +26,8 @@ int getOutputCount() {
 }
 
 void fpdf(I& ox, I& oy, I& oz, I& t, float inputs[], I outputsF[], I outputsDF[]) {
-outputsF[1] = (((((oz * oz) / 2.0f) + ((oy * oy) / 2.0f)) + (((ox + t) * (ox + t)) / 2.0f)) - 2.0f) * (((((oz * oz) / 2.0f) + ((oy * oy) / 2.0f)) + (((ox + t) * (ox + t)) / 2.0f)) - 2.0f); // 1.0
-outputsDF[1] = ((((((1.0f * (pow(t,3.0f))) + ((3.0f * ox) * (t * t))) + (((((1.0f * (oz * oz)) + (1.0f * (oy * oy))) + (3.0f * (ox * ox))) - 4.0f) * t)) + ((1.0f * ox) * (oz * oz))) + ((1.0f * ox) * (oy * oy))) + (1.0f * (pow(ox,3.0f)))) - (4.0f * ox); // 1.0
-outputsF[0] = ((((((((oz * oz) / 2.0f) - oz) + ((oy * oy) / 2.0f)) - oy) + (((ox + t) * (ox + t)) / 2.0f)) - (ox + t)) + 1.0f) * ((((((((oz * oz) / 2.0f) - oz) + ((oy * oy) / 2.0f)) - oy) + (((ox + t) * (ox + t)) / 2.0f)) - (ox + t)) + 1.0f); // 1.0
-outputsDF[0] = ((((((((((1.0f * (pow(t,3.0f))) + (((3.0f * ox) - 3.0f) * (t * t))) + ((((((((1.0f * (oz * oz)) - (2.0f * oz)) + (1.0f * (oy * oy))) - (2.0f * oy)) + (3.0f * (ox * ox))) - (6.0f * ox)) + 4.0f) * t)) + (((1.0f * ox) - 1.0f) * (oz * oz))) + ((2.0f - (2.0f * ox)) * oz)) + (((1.0f * ox) - 1.0f) * (oy * oy))) + ((2.0f - (2.0f * ox)) * oy)) + (1.0f * (pow(ox,3.0f)))) - (3.0f * (ox * ox))) + (4.0f * ox)) - 2.0f; // 1.0
+outputsF[1] = ((oz * oz / 2.0f + oy * oy / 2.0f + (ox + t) * (ox + t) / 2.0f) - 2.0f) * ((oz * oz / 2.0f + oy * oy / 2.0f + (ox + t) * (ox + t) / 2.0f) - 2.0f); // 1.0
+outputsDF[1] = (1.0f * pow(t,3.0f) + 3.0f * ox * t * t + ((1.0f * oz * oz + 1.0f * oy * oy + 3.0f * ox * ox) - 4.0f) * t + 1.0f * ox * oz * oz + 1.0f * ox * oy * oy + 1.0f * pow(ox,3.0f)) - 4.0f * ox; // 1.0
+outputsF[0] = (((oz * oz / 2.0f - oz + oy * oy / 2.0f) - oy + (ox + t) * (ox + t) / 2.0f) - (ox + t) + 1.0f) * (((oz * oz / 2.0f - oz + oy * oy / 2.0f) - oy + (ox + t) * (ox + t) / 2.0f) - (ox + t) + 1.0f); // 1.0
+outputsDF[0] = ((1.0f * pow(t,3.0f) + (3.0f * ox - 3.0f) * t * t + (((1.0f * oz * oz - 2.0f * oz + 1.0f * oy * oy) - 2.0f * oy + 3.0f * ox * ox) - 6.0f * ox + 4.0f) * t + (1.0f * ox - 1.0f) * oz * oz + (2.0f - 2.0f * ox) * oz + (1.0f * ox - 1.0f) * oy * oy + (2.0f - 2.0f * ox) * oy + 1.0f * pow(ox,3.0f)) - 3.0f * ox * ox + 4.0f * ox) - 2.0f; // 1.0
 }
