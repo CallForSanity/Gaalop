585bf37ca4556df6486e1e2f7bfc9a927b0cd9e7
pjuftring <pjuftring@users.noreply.github.com>
2019-10-28 18:42:36 +0100

    Add Plugins for Rust and Mathematica

---
+++ codegen-rust/src/main/java/de/gaalop/rust/Plugin.java
@@ -0,0 +1,51 @@
+package de.gaalop.rust;
+
+import de.gaalop.CodeGenerator;
+import de.gaalop.CodeGeneratorPlugin;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
+public class Plugin implements CodeGeneratorPlugin {
+
+    private Log log = LogFactory.getLog(Plugin.class);
+
+    private Image icon;
+
+    public Plugin() {
+        URL url = getClass().getResource("icon.png");
+        if (url != null) {
+            try {
+                icon = ImageIO.read(url);
+            } catch (IOException e) {
+                log.error("Unable to read plugin icon " + url);
+            }
+        } else {
+            log.warn("Unable to find plugin icon!");
+        }
+    }
+
+    @Override
+    public CodeGenerator createCodeGenerator() {
+        return RustCodeGenerator.INSTANCE;
+    }
+
+    @Override
+    public String getName() {
+        return "Rust";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This plugin generates Rust code.";
+    }
+
+    @Override
+    public Image getIcon() {
+        return icon;
+    }
+}
