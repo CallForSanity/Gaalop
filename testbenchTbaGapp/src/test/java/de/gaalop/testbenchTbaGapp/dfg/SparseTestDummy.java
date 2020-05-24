74713061707ef2a87818a42fecfe606aecdd7e04
CreaDisc <adrian.kiesthardt@gmail.com>
2020-05-24 17:53:55 +0200

    Adjusted message format again

--- testbenchTbaGapp/src/test/java/de/gaalop/testbenchTbaGapp/dfg/SparseTestDummy.java
+++ testbenchTbaGapp/src/test/java/de/gaalop/testbenchTbaGapp/dfg/SparseTestDummy.java
@@ -81,7 +81,7 @@ public class SparseTestDummy extends TestDummy {
             tBATestCase.testOutputs(outputVarsValues);
             return true;
         } catch (CompilationException ex) {
-            Logger.getLogger(TestDummy.class.getName()).log(Level.INFO, null, ex);
+            Logger.getLogger(TestDummy.class.getName()).log(Level.INFO, ex.getMessage());
             return false;
         }
 
--- testbenchTbaGapp/src/test/java/de/gaalop/testbenchTbaGapp/tba/TestDummySuite.java
+++ testbenchTbaGapp/src/test/java/de/gaalop/testbenchTbaGapp/tba/TestDummySuite.java
@@ -19,19 +19,15 @@ public class TestDummySuite {
         String message = "[Test] testing "+tBATestCase.getClass().getName();
 
         if(denseResult != sparseResult) {
-            message += "\n [WARNING]\t test with denseExpressions returned ";
+            message += "\n [WARNING]  unequal tests:" +
+                       "\n            test with denseExpressions returned ";
             message += denseResult ? "TRUE" : "FALSE";
-            message += " while sparseExpressions test returned ";
+            message += "\n            while sparseExpressions test returned ";
             message += sparseResult ? "TRUE" : "FALSE";
-        }
-
-        if(overallResult == false) {
-            message += "\n [WARNING]\t one test of this suite failed \n\t\t denseExpressions returned ";
-            message += denseResult ? "TRUE" : "FALSE";
-            message += "\n\t\t sparseExpressions test returned ";
-            message += sparseResult ? "TRUE" : "FALSE";
+        } else if(overallResult == false) {
+            message += "\n [INFO]     both tests of this suite failed";
         } else {
-            message += "\n SUCCESS both test succeeded with TRUE";
+            message += "\n SUCCESS:   both test succeeded with TRUE";
         }
         message += "\n";
 
